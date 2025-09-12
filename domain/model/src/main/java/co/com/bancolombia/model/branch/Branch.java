6486003f2eac33f49d4eec59917a076103270bb0
franciscovargas <fravarga@bancolombia.com.co>
2025-09-12 04:09:19 -0500

    Se crean los modelos de Franquicia, marca y producto

---
+++ domain/model/src/main/java/co/com/bancolombia/model/branch/Branch.java
@@ -0,0 +1,21 @@
+package co.com.bancolombia.model.branch;
+import co.com.bancolombia.model.product.Product;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Branch {
+    private String name;
+    private String address;
+    private List<Product> products = new ArrayList<>();
+}
---
+++ domain/model/src/main/java/co/com/bancolombia/model/franchise/Franchise.java
@@ -0,0 +1,21 @@
+package co.com.bancolombia.model.franchise;
+import co.com.bancolombia.model.branch.Branch;
+import lombok.Builder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+//import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+//@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Franchise {
+    private String id;
+    private String name;
+    private List<Branch> branches = new ArrayList<>();
+}
